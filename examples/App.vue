21c5968ec08503ef07af09f1eae61915ab8b1887
qxy <1329307562@qq.com>
2020-11-11 21:06:39 +0800

    1

--- examples/App.vue
+++ examples/App.vue
@@ -1,8 +1,38 @@
 <template>
   <div id="app">
-    <ls-button type="primary">登录</ls-button>
+    <ls-button type="primary" @click="login">登录</ls-button>
+    <ls-dialog :visible="dialogVisible" title="项目测试">
+      <ls-form :model="form" label-width="70px">
+        <ls-form-item label="用户名：">
+          <ls-input placeholder="请输入"></ls-input>
+        </ls-form-item>
+      </ls-form>
+      <template v-slot:footer>
+        <ls-button @click="confirmClick">确定</ls-button>
+      </template>
+    </ls-dialog>
   </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        dialogVisible: false,
+        form: {}
+      }
+    },
+    methods: {
+      login() {
+        this.dialogVisible = true;
+      },
+      confirmClick() {
+        this.dialogVisible = false;
+      }
+    }
+  }
+</script>
+
 <style>
+
 </style>
